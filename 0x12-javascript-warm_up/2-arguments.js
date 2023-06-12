ac5445dae6c42a1c3630af7e8266f3b4910fadb1
Jeffrey-Otu <otujeffrey02@gmail.com>
2023-06-12 16:50:59 -0700

    third task

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+printArg = (argument) => {
+	if (let countArg === 0){
+		console.log('No argument');
+	}else if(countArg === 1){
+		console.log('Argument found');
+	}else{
+		console.log('Arguments found');
+	};
+};
